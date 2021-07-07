422cc9505b621c684e7bd2f1a7fdcf54eec2592b
Perseo Durán <persioduran@gmail.com>
2021-07-06 19:28:41 -0500

    Mi primer proyexto

---
+++ prueba.php
@@ -0,0 +1,23 @@
+<?php
+//variables -comentario
+//ejemplo de variable
+
+$numero = 3.2;
+echo "este numero es: $numero" ; //la diferencia entre usar comillas dobles y comillas simplles es que las comillas simples le da un valor de texto.
+
+echo 'este numero es:' .$numero;
+
+//Constantes
+
+$curso  = 'php';
+
+echo $curso; 
+
+$curso = 'python';
+
+echo $curso;
+
+
+
+
+?>
